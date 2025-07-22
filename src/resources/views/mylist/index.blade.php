6b4e2a9d6ad02d2d71348b0c752c715183f7035e
craftbyhako <craftbyhako@gmail.com>
2025-07-22 14:17:54 +0900

    index画面の実装

--- src/resources/views/mylist/index.blade.php
+++ src/resources/views/mylist/index.blade.php
@@ -1,34 +1,35 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{asset('css/index.css')}}">
+<link rel="stylesheet" href="{{asset('css/index.css')}}?v={{ time() }}">
 <!-- <style>
 div {
   unicode-bidi: normal !important;
 }
 </style> -->
-
 @endsection
 
 
 @section('content')
-<ul>
-    <li><a class="index__menu" href="{{ url('/') }}">おすすめ</a></li>
-    <li><a class="index__menu" href="{{ url('/?tab=mylist') }}">マイリスト</a></li>
+<ul class="index__menu">
+    <li><a class="index__menu--item" href="{{ url('/') }}">おすすめ</a></li>
+    <li><a class="index__menu--item" href="{{ url('/?tab=mylist') }}">マイリスト</a></li>
 </ul>
 
 <div class="mylist">
     <div class="mylist__items">
         @foreach ($items as $item)
         <div class="mylist__item">
-            <!-- 画像 -->
+            
             <a href="{{ url('/item/' . $item->id) }}" class="item-link">
+                <!-- 画像 -->
                 <img src="{{ asset($item->image) }}" alt="商品画像" class="img-content">
+            
+                <!-- 商品名 -->
+                <div class="detail-content">
+                    <p class="detail-content__name">{{ $item->item_name }}</p>
+                </div>
             </a>
-            <!-- 商品名 -->
-            <div class="detail-content">
-                <p>{{ $item->name }}</p>
-            </div>
         </div>
         @endforeach
     </div>
