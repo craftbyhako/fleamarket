7de1520c19c4594dee3a5a80c05ce7b7d0cbc9b2
craftbyhako <craftbyhako@gmail.com>
2025-11-28 15:19:17 +0900

    取引チャット画面のレイアウト

--- src/resources/views/mylist/chat.blade.php
+++ src/resources/views/mylist/chat.blade.php
@@ -10,27 +10,39 @@
     <div class="chat__container">
 
         <aside class="chat__left-part">
-            <h3>その他の取引</h3>
+            <h2 class="chat__left-part--title">その他の取引</h2>
             @foreach($pendingTrades as $pendingTrade)
-                @if(in_array($pendingTrade->status, [1, 2]))
-                    <a href="{{ route('chat.show', $pendingTrade->id) }}">
-                        <p>{{ $pendingTrade->item->item_name }}</p>
-                    </a>
-                @endif
+            @if(in_array($pendingTrade->status, [1, 2]))
+            <a href="{{ route('chat.show', $pendingTrade->id) }}">
+                <p class="chat__left-part--pending_trades">{{ $pendingTrade->item->item_name }}</p>
+            </a>
+            @endif
             @endforeach
         </aside>
 
 
         <div class="chat__right-part">
             <div class="chat__header">
-                <img src="{{ asset('storage/' . $otherUser->profile_image) }}" alt="{{ $otherUser->user_name }}のプロフィール画像">
-                <p>{{ $otherUser->user_name }}さんとの取引画面 </p>
+
+                <div class="chat__header--counterparty">
+                    @if ($otherUser->profile_image && Storage::disk('public')->exists($otherUser->profile_image))
+                        <img class="chat__header--counterparty-image"
+                            src="{{ asset('storage/' . $otherUser->profile_image) }}"
+                            alt="{{ $otherUser->user_name }}のプロフィール画像">
+                    @else
+                        <div class="chat__header--placeholder">
+                            {{ mb_substr($otherUser->user_name, 0, 1) }}
+                        </div>
+                    @endif
+
+                    <p class="chat__header--counterparty-title">{{ $otherUser->user_name }}さんとの取引画面 </p>
+                </div>
                 <button class="chat__complete-button" type="submit">取引を完了する</button>
             </div>
 
             <div class="chat__bought-item">
-                <div class="chat__bought-item-wrapper">
-                    <img src="{{ asset('storage/' . $trade->item->image) }}" alt="商品画像">
+                <div class="chat__bought-item--wrapper">
+                    <img class="chat__bought-item--image" src="{{ asset('storage/' . $trade->item->image) }}" alt="商品画像">
                     <h2>{{ $trade->item->item_name }}</h2>
                     <p>{{ $trade->item->price }}</p>
                 </div>
@@ -39,19 +51,19 @@
             <div class="chat__messages">
                 <div class="chat__messages-content">
                     @foreach($messages as $message)
-                        <div class="{{ $message->is_me ? 'chat__messages-content--sent' : 'chat__message-content--received' }}">
-                            <img class="chat__messages--profile-image" src="{{ asset('storage/' . $message->display_image) }}" alt="プロフィール画像">
-                            <p>{{ $message->display_name }}</p>
-                            <textarea class="chat__messages--text">{{ $message->message }}</textarea>
+                    <div class="{{ $message->is_me ? 'chat__messages-content--sent' : 'chat__message-content--received' }}">
+                        <img class="chat__messages--profile-image" src="{{ asset('storage/' . $message->display_image) }}" alt="プロフィール画像">
+                        <p>{{ $message->display_name }}</p>
+                        <textarea class="chat__messages--text">{{ $message->message }}</textarea>
 
-                            @if ($message->is_me)
-                                <a href="">編集</a>
-                                <a href="">削除</a>
-                            @endif
-                        </div>
+                        @if ($message->is_me)
+                        <a href="">編集</a>
+                        <a href="">削除</a>
+                        @endif
+                    </div>
                     @endforeach
                 </div>
-                
+
                 <div class="chat__messages--create">
                     <form class="chat__messages--create-form" action="{{ route('chat.store', $trade->id) }}" method="POST">
                         @csrf
