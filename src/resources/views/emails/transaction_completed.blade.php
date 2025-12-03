a56b3bbb1613f1af70d29bc3de934ce823977edd
craftbyhako <craftbyhako@gmail.com>
2025-12-03 15:08:23 +0900

    mailhogの実装

---
+++ src/resources/views/emails/transaction_completed.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<body>
+    <!-- 出品者 -->
+    <p>{{ $item->user->user_name }}様</p>
+
+    <p>いつもFleamarketをご利用いただき、誠にありがとうございます。</p>
+    <br>
+    <br>
+    <p>購入者：{{ $buyer->user_name }}さんが、<br>
+        あなたの商品「　{{ $item->item_name }}　」のお取引を完了しました。
+    </p>
+    <br>
+    <br>
+    <p>ログインの上、お取引画面にて購入者の評価をお願いします。</p>
+
+</body>
+
+</html>
